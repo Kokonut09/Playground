212257c88ed2c8e8166687b6aa5ff9b4ca1482a8
Kokonut09 <a09saeyang@gmail.com>
2020-02-16 23:52:43 -0800

    Initial logic

--- src/YahtzeeScoring.java
+++ src/YahtzeeScoring.java
@@ -8,7 +8,39 @@ public class YahtzeeScoring {
 
 	public static void main(String[] args) {
 		
+		int[] roll = {2,3,5,5,6};
+		
+		
+		
+		System.out.println(yahtzeeUpper(roll));
 
+	}
+
+	private static int yahtzeeUpper(int[] roll) {
+		
+		int yHigh = 0;
+		int yCount = 0;
+		
+		for(int i = 0; i<roll.length; i++) {
+			int count = 0;
+			int highNums = 0;
+			
+			for(int j =0; j<roll.length;j++) {
+				
+				if (roll[i] == roll[j]) {
+					count++;
+				}
+				
+			}
+			if(count >= yHigh) {
+				yHigh = i;
+				yCount = count;
+			}
+			
+			
+		}
+		
+		return yHigh*yCount;
 	}
 
 }
