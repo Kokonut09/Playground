8988148c47032bf49da64cc9b2b0e15db38e5369
Kokonut09 <a09saeyang@gmail.com>
2020-02-16 23:35:00 -0800

    First Yahtzee Commit

---
+++ src/YahtzeeScoring.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * https://www.reddit.com/r/dailyprogrammer/comments/dv0231/20191111_challenge_381_easy_yahtzee_upper_section/
+ * @author a09sa
+ *
+ */
+public class YahtzeeScoring {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
