6ff0e20c72e474014e0707047c495312c6837287
Kokonut09 <a09saeyang@gmail.com>
2020-02-17 23:59:03 -0800

    tax brackets?

--- src/ProgressiveTaxation.java
+++ src/ProgressiveTaxation.java
@@ -1,9 +1,35 @@
-
+/**
+ * https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/
+ * @author a09sa
+ *
+ */
 public class ProgressiveTaxation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+		int income = 12000;
+		System.out.println(taxIt(income));
+		
 
 	}
+
+	private static int taxIt(int income) {
+		
+		int taxBracket1 = 0;
+		int taxBracket2 = 10;
+		int taxBracket3 = 25;
+		int taxBracket4 = 40;
+		
+		int tax = 0;
+	
+		
+		//If >100000 - income-100000 *.40
+		
+		
+		return tax;
+	}
+	
+	
 
 }
