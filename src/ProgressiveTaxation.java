3eedbbd6e0d5d0daec2ce7fafaacc0075cefc7a5
Kokonut09 <a09saeyang@gmail.com>
2020-02-18 21:55:24 -0800

    Tax Bracket logic completed

--- src/ProgressiveTaxation.java
+++ src/ProgressiveTaxation.java
@@ -6,30 +6,47 @@
 public class ProgressiveTaxation {
 
 	public static void main(String[] args) {
-	
-		
-		int income = 12000;
+
+
+		int income = 1234567;
 		System.out.println(taxIt(income));
-		
+
 
 	}
 
+	/**
+	 * Income over the threshold will be static. Example. income of 40,000 or 50,000 will have
+	 * the 2000 tax from the previous tax bracket, then you will only need to calculate the remainder
+	 * with the appropriate tax bracket 
+	 * @param income
+	 * @return
+	 */
 	private static int taxIt(int income) {
-		
+
 		int taxBracket1 = 0;
 		int taxBracket2 = 10;
 		int taxBracket3 = 25;
 		int taxBracket4 = 40;
-		
+
 		int tax = 0;
-	
+
+
+		if(income > 100000) {
 		
-		//If >100000 - income-100000 *.40
-		
-		
+			tax = (income - 100000) * taxBracket4/100 + 2000 + 17500;
+
+		}else if(income>30000) {
+
+			tax = (income - 30000) * taxBracket3/100 + 2000;
+
+		}else if(income>10000) {
+
+			tax = (income-10000) * taxBracket2 /100; 
+		}
+
 		return tax;
 	}
-	
-	
+
+
 
 }
