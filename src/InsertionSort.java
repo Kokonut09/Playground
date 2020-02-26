cd34360ea0e99720bccf648f93fc485b0814ae5a
Kokonut09 <a09saeyang@gmail.com>
2020-02-25 21:43:06 -0800

    Insertion Sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -2,18 +2,27 @@
 public class InsertionSort {
 
 	public static void main(String[] args) {
+		
 		int[] arr = {7, 2, 6, 6, 9, 6, 6, 6, 2, 0};
 
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = 1; i < arr.length; i++) {
 			
-			for(int k = i; k >0; k--) {
+			int key = arr[i];
+			int k = i - 1;
+			while(k >= 0 && key < arr[k]) {
 				
+				int temp = arr[k];
+				arr[k] = arr[k+1];
+				arr[k+1] = temp;
+				k--;
 				//System.out.println(k);
 				
 				
 			}
 			
 		}
+		
+		TestClass.arrPrint(arr);
 	}
 
 }
