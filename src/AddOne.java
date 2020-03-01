fea59f0a5f20955ac5168175044dbfce1f208383
Andrew Saeyang <a09saeyang@gmail.com>
2020-02-29 19:19:39 -0800

    WIP printing in the wrong direction

--- src/AddOne.java
+++ src/AddOne.java
@@ -5,23 +5,51 @@ public class AddOne {
 
 	public static void main(String[] args) {
 
-		int nums = 7;
-		
+		int nums = 1234;
+
 		System.out.println(addOne(nums));
-		
+
 	}
 
-	private static int addOne(int nums) {
-		int retVal = 0;
-		
-		if(nums < 9) {
-			retVal = nums+1;
+	private static String addOne(int nums) {
+
+		String retVal = "";
+
+		if(nums < 10) {
 			
-		}else
-			retVal = 10;
-		
-		
-		return retVal;
+			return retVal += (nums+1);
+			
+		}else {
+			int temp = 0;
+			if(nums<1000000 && nums >= 100000) {
+
+				temp = nums / 100000;
+				
+			}else if(nums <100000 && nums >= 10000) {
+				
+				temp = nums / 10000;
+
+				
+			}else if(nums <10000 && nums >= 1000) {
+				
+				temp = nums / 1000;
+
+				
+			}else if(nums <1000 && nums >= 100) {
+				
+				temp = nums / 100;
+
+				
+			}else if(nums <100 && nums >= 10) {
+				
+				temp = nums / 10;
+
+			}
+			return retVal += (temp+1) + addOne(nums/10) ;
+		}
+
+
+
 	}
 
 }
